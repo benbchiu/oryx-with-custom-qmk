b411a61f1be6a845e7c651530b893cccb0e19438
benbchiu <benbchiu@gmail.com>
2025-01-22 07:40:23 +0800

    Update keymap.c

--- 34eXB/keymap.c
+++ 34eXB/keymap.c
@@ -8,8 +8,8 @@ enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
 };
 
-const key_override_t lprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_LBRC);
-const key_override_t rprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_RPRN, KC_RBRC);
+const key_override_t lprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LBRC, KC_LPRN);
+const key_override_t rprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_RBRC, KC_RPRN);
 
 // This globally defines all key overrides to be used
 const key_override_t *key_overrides[] = {
