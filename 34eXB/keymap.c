ee83cade628e7fd640091af949f6b5b5ad9653e7
benbchiu <benbchiu@gmail.com>
2025-01-21 23:23:03 +0800

    Update keymap.c

--- 34eXB/keymap.c
+++ 34eXB/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
+#include "features/achordion.h"
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
 
@@ -7,7 +8,18 @@ enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
 };
 
+const key_override_t lprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_LBRACKET);
+const key_override_t rprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_RPRN, KC_RBRACKET);
 
+// This globally defines all key overrides to be used
+const key_override_t *key_overrides[] = {
+	&lprn_key_override,
+	&rprn_key_override
+};
+
+void housekeeping_task_user(void) {
+  achordion_task();
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
@@ -91,6 +103,8 @@ bool rgb_matrix_indicators_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_achordion(keycode, record)) { return false; }
+
   switch (keycode) {
 
     case RGB_SLD:
